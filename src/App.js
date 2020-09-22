e3ea03304cdeaca04efd5cfdbb74fc4a8bc4437b
Quackenstien <donaldhawkins422@gmail.com>
2020-09-22 09:48:48 -0700

    added black circle

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
   let circle = useRef(null);
   let circleRed = useRef(null);
   let circleBlue = useRef(null);
+  let circleBlack = useRef(null);
 
   const [toggleCircle, setToggleCircle] = useState(false);
 
-  const handleExpand = () => {
+  const handleRedCircle = () => {
     TweenMax.to(circleRed, 2, {
       width: 200,
       height: 200,
@@ -19,8 +20,61 @@ function App() {
     setToggleCircle(true);
   };
 
-  const handleShrink = () => {
+  const handleRedCircle1 = () => {
     TweenMax.to(circleRed, 2, {
+      width: 75,
+      height: 75,
+      ease: Power3.easeOut,
+    });
+    setToggleCircle(false);
+  };
+
+  const handleYellowCircle = () => {
+    TweenMax.to(circle, 2, {
+      width: 200,
+      height: 200,
+      ease: Power3.easeOut,
+    });
+    setToggleCircle(true);
+  };
+
+  const handleYellowCircle1 = () => {
+    TweenMax.to(circle, 2, {
+      width: 75,
+      height: 75,
+      ease: Power3.easeOut,
+    });
+    setToggleCircle(false);
+  };
+
+  const handleBlueCircle = () => {
+    TweenMax.to(circleBlue, 2, {
+      width: 200,
+      height: 200,
+      ease: Power3.easeOut,
+    });
+    setToggleCircle(true);
+  };
+
+  const handleBlackCircle1 = () => {
+    TweenMax.to(circleBlack, 2, {
+      width: 75,
+      height: 75,
+      ease: Power3.easeOut,
+    });
+    setToggleCircle(false);
+  };
+  const handleBlackCircle = () => {
+    TweenMax.to(circleBlack, 2, {
+      width: 200,
+      height: 200,
+      ease: Power3.easeOut,
+    });
+    setToggleCircle(true);
+  };
+
+  const handleShrinkAll = () => {
+    TweenMax.to([circleRed, circle, circleBlue, circleBlack], 2, {
       width: 75,
       height: 75,
       ease: Power3.easeOut,
@@ -47,7 +101,7 @@ function App() {
 
     TweenMax.to(app, 0, { css: { visibility: "visible" } }); //This along with setting .app to hidden will let the DOM load then run the animations to allow for a smooth transition.
     TweenMax.staggerFrom(
-      [circle, circleRed, circleBlue],
+      [circle, circleRed, circleBlue, circleBlack],
       2,
       { opacity: 0, x: 100, ease: Power3.easeOut },
       0.2
@@ -57,14 +111,33 @@ function App() {
   return (
     <div className="App" ref={(el) => (app = el)}>
       <header className="App-header">
+        <h1>Click on the circles to see some magic!!! </h1>
+
         <div className="circle-container">
-          <div className="circle" ref={(el) => (circle = el)}></div>
+          <div
+            className="circle"
+            ref={(el) => (circle = el)}
+            onClick={
+              toggleCircle !== true ? handleYellowCircle : handleYellowCircle1
+            }
+          ></div>
           <div
             className="circle red"
             ref={(el) => (circleRed = el)}
-            onClick={toggleCircle !== true ? handleExpand : handleShrink}
+            onClick={toggleCircle !== true ? handleRedCircle : handleRedCircle1}
           ></div>
-          <div className="circle blue" ref={(el) => (circleBlue = el)}></div>
+          <div
+            className="circle blue"
+            ref={(el) => (circleBlue = el)}
+            onClick={toggleCircle !== true ? handleBlueCircle : handleShrinkAll}
+          ></div>
+          <div
+            className="circle black"
+            ref={(el) => (circleBlack = el)}
+            onClick={
+              toggleCircle !== true ? handleBlackCircle : handleBlackCircle1
+            }
+          ></div>
         </div>
       </header>
     </div>
